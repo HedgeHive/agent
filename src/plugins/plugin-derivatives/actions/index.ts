26cd6c054bad94ae92895468b0f6908541466848
Ali Nuraldin <alirunwwc@gmail.com>
2025-02-06 16:06:20 +0100

    Refactored market data actions

--- src/plugins/plugin-derivatives/actions/index.ts
+++ src/plugins/plugin-derivatives/actions/index.ts
@@ -7,6 +7,46 @@ import { createAction } from "../../../goat/adapters/eliza.ts";
 import { getDeribitSingleton } from "../services/deribit-api/index.ts";
 import { DeribitCurrency } from "../services/deribit-api/types.ts";
 
+const getActiveInstruments = async (underlyingAsset: DeribitCurrency) => {
+  const deribit = getDeribitSingleton();
+  const instruments = await deribit.getInstruments(underlyingAsset, 'option', false);
+  return instruments.filter(i => i.is_active);
+}
+
+const assertAndParseUnderlyingAsset = (underlyingAsset: string) => {
+  assert(underlyingAsset, "Underlying asset is required")
+  if (underlyingAsset !== 'any') {
+    underlyingAsset = underlyingAsset.toUpperCase()
+  }
+  return underlyingAsset as DeribitCurrency
+}
+
+const assertAndParseInstrumentName = (instrumentName: string) => {
+  assert(instrumentName, "Instrument name is required")
+  instrumentName = instrumentName.toUpperCase()
+  return instrumentName
+}
+
+export const CheckIfOptionIsAvailable = createAction(createTool(
+  {
+    name: "check_if_option_is_available",
+    description: "Checks the availability of the option with the given instrument <ASSET>-<DMMMYY>-<STRIKE>-<C|P>",
+    parameters: z.object({
+      instrumentName: z.string({ description: "Instrument name in format <ASSET>-<DMMMYY>-<STRIKE>-<C|P>" }),
+      underlyingAsset: z.string({ description: "Underlying asset ('BTC' | 'ETH' | 'USDC' | 'USDT' | 'EURR' | 'any')" }),
+    }),
+  },
+  async (parameters) => {
+    elizaLogger.info({ parameters })
+
+    const instrumentName = assertAndParseInstrumentName(parameters.instrumentName)
+    const underlyingAsset = assertAndParseUnderlyingAsset(parameters.underlyingAsset)
+
+    const activeInstruments = await getActiveInstruments(underlyingAsset)
+    return activeInstruments.map(i => i.instrument_name).includes(instrumentName)
+  }
+))
+
 export const GetAvailableOptions = createAction(createTool(
   {
     name: "get_available_options",
@@ -17,16 +57,11 @@ export const GetAvailableOptions = createAction(createTool(
   },
   async (parameters) => {
     elizaLogger.info({ parameters })
-    assert(parameters.underlyingAsset, "Underlying asset is required")
 
-    let underlyingAsset = parameters.underlyingAsset
-    if (underlyingAsset !== 'any') {
-      underlyingAsset = underlyingAsset.toUpperCase()
-    }
+    const underlyingAsset = assertAndParseUnderlyingAsset(parameters.underlyingAsset)
 
-    const deribit = getDeribitSingleton();
-    const instruments = await deribit.getInstruments(underlyingAsset as DeribitCurrency, 'option', false);
-    return instruments.filter(i => i.is_active).map(i => i.instrument_name);
+    const activeInstruments = await getActiveInstruments(underlyingAsset)
+    return activeInstruments.map(i => i.instrument_name);
   }
 ))
 
@@ -35,18 +70,19 @@ export const GetOptionPriceAction = createAction(createTool(
     name: "get_option_price",
     description: "Get the market price of the given option instrument <ASSET>-<DMMMYY>-<STRIKE>-<C|P>",
     parameters: z.object({
-      instrumentName: z.string({ description: "Instrument name in format <ASSET>-<DMMMYY>-<STRIKE>-<C|P>" }),
+      instrumentName: z.string({ description: "Instrument name in format <ASSET>-<DMMMYY>-<STRIKE>-<C|P>" })
     }),
   },
   async (parameters) => {
     elizaLogger.info({ parameters })
-    assert(parameters.instrumentName, "Instrument name is required")
 
-    const instrumentName = parameters.instrumentName.toUpperCase()
+    const instrumentName = assertAndParseInstrumentName(parameters.instrumentName)
 
     const deribit = getDeribitSingleton();
     const instrumentSummary = await deribit.getBookSummaryByInstrument(instrumentName)
+
     elizaLogger.info({ instrumentSummary })
+
     return instrumentSummary.mark_price
   }
 ))
--- src/plugins/plugin-derivatives/index.ts
+++ src/plugins/plugin-derivatives/index.ts
@@ -1,11 +1,12 @@
 import { Plugin } from "@elizaos/core";
 import { AvailableUnderlyingAssetsProvider, IndexPricesProvider } from "./providers/index.ts";
-import { GetAvailableOptions, GetOptionPriceAction } from "./actions/index.ts";
+import { CheckIfOptionIsAvailable, GetAvailableOptions, GetOptionPriceAction } from "./actions/index.ts";
 
 export const derivativesPlugin: Plugin = {
   name: "derivatives",
   description: "Plugin that enables derivatives trading",
   actions: [
+    CheckIfOptionIsAvailable,
     GetAvailableOptions,
     GetOptionPriceAction
   ],
